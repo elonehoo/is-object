c7ba55eaf3265b1cd301f10de664f65377dfe714
elonehoo <huchengyea@163.com>
2022-10-27 20:35:22 +0800

    test:add test code

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,31 @@
 import { describe, expect, it } from 'vitest'
+import isObject from '../src/index'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+  it('returns true for objects', () => {
+    expect(true).toEqual(isObject({}))
+    expect(true).toEqual(isObject([]))
+  })
+
+  it('returns false for null',()=>{
+    expect(false).toEqual(isObject(null))
+  })
+
+  it('returns false for undefined',()=>{
+    expect(false).toEqual(isObject(undefined))
+  })
+
+  it('returns false for booleans',()=>{
+    expect(false).toEqual(isObject(true))
+    expect(false).toEqual(isObject(false))
+  })
+
+  it('returns false for primitives',()=>{
+    expect(false).toEqual(isObject(42))
+    expect(false).toEqual(isObject('bar'))
+  })
+
+  it('returns false for functions',()=>{
+    expect(false).toEqual(isObject(function() {}))
   })
 })
